916e21973741f2e63655661db9029be03de9c58b
unknown <marianasevarollisenai@gmail.com>
2023-04-14 14:29:27 -0300

    Neste arquivo foi feito um programa que recebe 5 números e informa qual dentro dos 5 é o maior número.

---
+++ exercicio7repeticao.py
@@ -0,0 +1,9 @@
+num1 = float(input("Número 1: "))
+num2 = float(input("Número 2: "))
+num3 = float(input("Número 3: "))
+num4 = float(input("Número 4: "))
+num5 = float(input("Número 5: "))
+
+lista = [num1, num2, num3, num4, num5]
+
+print("O maior número é: ", max(lista))
