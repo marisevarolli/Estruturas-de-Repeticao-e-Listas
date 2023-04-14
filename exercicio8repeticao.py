3afc16c258274f6133c49145dc79a133a3d823f8
unknown <marianasevarollisenai@gmail.com>
2023-04-14 14:30:34 -0300

    Neste arquivo foi feito um programa que receber 5 números e informa a soma e a média deles.

---
+++ exercicio8repeticao.py
@@ -0,0 +1,10 @@
+num1 = float(input("Número 1: "))
+num2 = float(input("Número 2: "))
+num3 = float(input("Número 3: "))
+num4 = float(input("Número 4: "))
+num5 = float(input("Número 5: "))
+
+soma = num1 + num2 + num3 + num4 + num5
+media = soma / 5
+
+print("Soma: ", soma, "\nMedia: ", media)
