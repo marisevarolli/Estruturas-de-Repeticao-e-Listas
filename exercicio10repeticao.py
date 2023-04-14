9c63498e91e0b97a7015f6a043b1c518347f1562
unknown <marianasevarollisenai@gmail.com>
2023-04-14 14:33:27 -0300

    Neste arquivo foi feito um programa que recebe dois números inteiros e aponta quais são os números que estão no intervalo entre os números indicados.

---
+++ exercicio10repeticao.py
@@ -0,0 +1,7 @@
+num1 = int(input("Digite um número inteiro: "))
+num2 = int(input("Digite outro número inteiro: "))
+
+for m in range(num1 + 1, num2):
+        print(m)
+for m in range(num2 + 1, num1):
+        print(m)
